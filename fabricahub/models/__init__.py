08a20c4035930d946905608a5163c61fdfd367dc
Luís Felipe <luis.felipe.ferreira.da.silva.if@gmail.com>
2026-05-20 18:04:07 +0000

    feat: Adicionando verbose_name para a model Task para melhor legibilidade no admin e retornando o título e status da tarefa. E apontando task em models

--- fabricahub/models/__init__.py
+++ fabricahub/models/__init__.py
@@ -3,11 +3,12 @@ from .membro import Membro
 from .performance_log import PerformanceLog
 from .projeto import Projeto
 from .usuario import Usuario
-
+from .task import Task
 __all__ = [
     "BaseModel",
     "Usuario",
     "Membro",
     "Projeto",
     "PerformanceLog",
+    "Task",
 ]
--- fabricahub/models/task.py
+++ fabricahub/models/task.py
@@ -11,3 +11,9 @@ class Task(BaseModel):
     prazo = models.DateField(null=True, blank=True)
     projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
     responsavel = models.ForeignKey(Membro, on_delete=models.SET_NULL, null=True, blank=True)
+    class Meta:
+        verbose_name = "Tarefa"
+        verbose_name_plural = "Tarefas"
+        ordering = ["prazo", "projeto", "status"]
+    def __str__(self):
+        return f"{self.titulo} - {self.get_status_display()}"
