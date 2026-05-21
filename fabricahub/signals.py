c47cedb89e41be70a3033f700ea776e48d069d23
Luís Felipe <luis.felipe.ferreira.da.silva.if@gmail.com>
2026-05-21 13:43:11 -0300

    feat: Quando o Projeto for concluido, os membros vinculados (ProjetoMembro) tera a data fim atualizada

--- fabricahub/signals.py
+++ fabricahub/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from fabricahub.models import Task, LogAuditoria
+from fabricahub.models import Task, LogAuditoria, Projeto, ProjetoMembro
 
 @receiver(post_save, sender=Task)
 def auditar_task(sender, instance, created, **kwargs):
@@ -17,3 +17,15 @@ def auditar_task(sender, instance, created, **kwargs):
         entidade_id=instance.id,
         detalhes=f"A Tarefa '{instance.titulo}' vinculada ao projeto '{instance.projeto}' com o status '{instance.get_status_display()}'."
     )
+
+@receiver(post_save, sender=Projeto)
+def encerrar_projeto_finalizado(sender, instance, created, **kwargs):
+    """
+    Caso o projeto mude de status para 'Finalizado', 
+    atualiza a data_fim de todos os vínculos na tabela intermediária.
+    """
+    if instance.status == Projeto.Status.FINALIZADO:
+        ProjetoMembro.objects.filter(
+            projeto=instance.id,
+            data_fim__isnull=True
+        ).update(data_fim=timezone.now().date())
