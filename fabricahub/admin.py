541d33e582d5c371ee7f7caaa41bcc119240e58f
Luís Felipe <luis.felipe.ferreira.da.silva.if@gmail.com>
2026-05-22 15:44:55 +0000

    Removendo ID do admin (display) visualmente fica mais legivel

--- fabricahub/admin.py
+++ fabricahub/admin.py
@@ -5,7 +5,7 @@ from fabricahub.models import Membro, Projeto, Usuario, LogAuditoria, Task, Proj
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "first_name", "last_name", "email", "tipo_usuario", "is_active", "is_staff", "is_superuser", "last_login")
+    list_display = ("username", "first_name", "last_name", "email", "tipo_usuario", "is_active", "is_staff", "is_superuser", "last_login")
     # search_fields = ("username", "first_name", "last_name", "email")
     fieldsets = (
         ("Informações de Credenciais", {
@@ -24,29 +24,29 @@ class UsuarioAdmin(admin.ModelAdmin):
 
 @admin.register(Membro)
 class MembroAdmin(admin.ModelAdmin):
-    list_display = ("id", "usuario", "curso", "semestre", "squad")
+    list_display = ("usuario", "curso", "semestre", "squad")
     search_fields = ("usuario__username", "usuario__first_name", "usuario__last_name", "curso")
     list_filter = ("squad", "semestre")
 
 
 @admin.register(Projeto)
 class ProjetoAdmin(admin.ModelAdmin):
-    list_display = ("id", "nome", "status", "data_inicio", "prazo_entrega")
+    list_display = ("nome", "status", "data_inicio", "prazo_entrega")
     search_fields = ("nome",)
     list_filter = ("status",)
 
 
 @admin.register(LogAuditoria)
 class LogAuditoriaAdmin(admin.ModelAdmin):
-    list_display = ("id", "operador", "acao", "entidade_afetada", "created_at")
+    list_display = ("operador", "acao", "entidade_afetada", "created_at")
     search_fields = ("operador__username", "acao", "entidade_afetada")
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("id", "titulo", "descricao", "status", "projeto", "responsavel")
+    list_display = ("titulo", "descricao", "status", "projeto", "responsavel")
     search_fields = ("titulo", "descricao")
     #
 @admin.register(ProjetoMembro)
 class ProjetoMembroAdmin(admin.ModelAdmin):
-    list_display = ("id", "projeto", "membro")
+    list_display = ("projeto", "membro", "data_entrada", "data_saida")
     search_fields = ("projeto__nome", "membro__usuario__username")
